371dd8eb10ef9ce274277ad19cb1d3c50f3dd89f
Manan Computer <sami606713@gmail.com>
2024-09-09 11:19:03 -0700

    web app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # ====================================Import Packages===============================#
 import os
-from utils import load_documents, create_vector_store
+from utils import (load_documents, create_vector_store,
+                   create_rag_chain,get_response,
+                   load_previous_chat,stream_data,save_notes)
 from langchain_cohere import CohereEmbeddings,ChatCohere
 from dotenv import load_dotenv
+import streamlit as st
 load_dotenv()
 # ================================================================================#
 
@@ -12,15 +15,28 @@ load_dotenv()
 # 3- Create Embedding
 # 4- Save  the embedding in vector db
 # 5- Make the retriever
+# =====================================Set the title and Page configration=============================#
+if "chat_history" not  in st.session_state:
+    st.session_state['chat_history']=[]
+# Set the page configuration
+st.set_page_config(
+    page_title="Document Question Answering",  # Title of the web page
+    page_icon="🎥",  # Icon for the web page (you can use any emoji)
+    layout="centered",  # Layout of the page (either 'centered' or 'wide')
+    initial_sidebar_state="auto"  # Initial state of the sidebar ('auto', 'expanded', 'collapsed')
+)
+
+# Set the page title
+st.title("Documents Quesion Answering")
 
 #====================================Load Documents================================#
-file_path="Doc/OS.pdf"
+folder_path="Doc"
 vector_store_path="db"
 
-
-# doc_chunks=load_documents(file_path=file_path)
-# print("========Total Chunks========\n",len(doc_chunks))
-# print("========First Chunk========\n",doc_chunks[0])
+print("=====Loading Files======")
+doc_chunks=load_documents(folder_path=folder_path)
+print("========Total Chunks========\n",len(doc_chunks))
+print("========First Chunk========\n",doc_chunks[0])
 
 
 #====================================Create Vector Store================================#
@@ -29,7 +45,7 @@ try:
     embedding=CohereEmbeddings(model="embed-english-light-v3.0",cohere_api_key=os.environ['cohere_api'])
     print("=====Creating the vector store=======")
     # temp chunk
-    doc_chunks="i am samiullah"
+    # doc_chunks="i am samiullah"
     vector_store=create_vector_store(embedding=embedding,doc_chunks=doc_chunks,
                                      vectcor_store_path=vector_store_path)
     
@@ -40,7 +56,28 @@ except Exception as e:
     print(e)
 
 #====================================Chat================================#
-query="What is the operating system?"
-context=retriever.invoke(input=query)
+# query="What is the operating system?"
+# context=retriever.invoke(input=query)
 
-print("========Context========\n",context[0].page_content)
+# print("========Context========\n",context[0].page_content)
+
+# Load the llm
+llm=ChatCohere(cohere_api_key=os.environ['cohere_api'])
+
+# create a rag chain
+rag_chain=create_rag_chain(llm=llm,retriever=retriever)
+
+# Get response
+prompt=st.chat_input(placeholder="Enter you question? ")
+
+with st.container(border=False):
+    load_previous_chat()
+
+if prompt:
+    st.write(f'💬 You: {prompt}') 
+    save_notes("Doc/Note_os.docx",f"Q: {prompt+'\n'}")
+    response = get_response(rag_chain, prompt)
+    st.write_stream(stream_data(response))
+
+    # Save notes
+    save_notes("Doc/Note_os.docx",f"Ans: {response+'\n'}")
--- utils.py
+++ utils.py
@@ -9,18 +9,34 @@ from langchain.chains.history_aware_retriever import create_history_aware_retrie
 from langchain.chains.retrieval import create_retrieval_chain
 import streamlit as st
 from dotenv import load_dotenv
+import time
 import os
 # ================================================================================#
 
 # Load Document
-def load_documents(file_path:str):
+def load_documents(folder_path:str):
     """
     This fun is responsible for loading the documents
     """
     # load the documents
     print("======Laoding Docuemts======")
-    loader= PyPDFLoader(file_path)
-    documents=loader.load()
+    files= os.listdir(folder_path)
+    documents=[]
+    for file in files:
+        if file.endswith(".pdf"):
+            print(f"========={file}========")
+            curr_doc=os.path.join(folder_path,file)
+             # load the document
+            print(f"Loading Document===={file}")
+            text_loader=PyPDFLoader(file_path=curr_doc)
+            docs=text_loader.load()
+
+            # Adding meta data to the document
+            print("======Adding Meta data")
+            for doc in docs:
+                doc.metadata={"source":file,"type":"PDF","name":file.split()[0]}
+                documents.append(doc)
+
     
     # Split the text into chunks
     print("======Splitting the text into chunks======")
@@ -48,3 +64,105 @@ def create_vector_store(embedding,doc_chunks,vectcor_store_path:str):
         vector_store=FAISS.load_local(folder_path=vectcor_store_path,embeddings=embedding,
                                       allow_dangerous_deserialization=True)
         return vector_store
+
+
+def create_rag_chain(llm,retriever):
+    try:
+        # make a sytem message
+        system_message = (
+            "You are given a chat history and a new user question. "
+            "Your task is to review both the chat history and the new question. "
+            "If the new question references context from the chat history, reformulate it "
+            "to ensure it is standalone and comprehensible. If the new question does not "
+            "need any modification and is clear on its own, return it as is. "
+            "Do not answer the question—focus solely on reformulating it for clarity if necessary."
+        )
+
+        # cousomize the prompt
+        coustomize_system_prompt=ChatPromptTemplate.from_messages(
+            [
+                ("system",system_message),
+                MessagesPlaceholder("chat_history"),
+                ("human","{input}")
+            ]
+        )
+
+        # set the history aware retriever
+        history_aware_retriever=create_history_aware_retriever(
+            llm=llm,retriever=retriever,prompt=coustomize_system_prompt
+        )
+
+        # make a question_answer prompt
+        q_a_prompt = (
+            "You are an expert assistant for Opreating System Question Anwering. "
+            "Please use the following context to formulate your response to the user’s query. "
+            "If the provided context does not allow you to answer the question, respond with 'I don't know.' "
+            "if question is not related to math and calculus subject answer should be return in this format.\n"
+            "Defination:\n define with in 3 lines.\nExplanation:\n Explain them in bullet point format.\nExample\n"
+            "Give the answer in word doc format."
+            "Note that answer is not too long keep the answer consise and clear and make its easier for preparation."
+            "{context}"
+        )
+
+
+        # coustoize the Q&A prompt
+        final_qa_prompt=ChatPromptTemplate(
+            [
+                ("system",q_a_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human","{input}")
+            ]
+        )
+
+        # create a document stuff
+        question_answer_chain=create_stuff_documents_chain(llm,final_qa_prompt)
+
+        # make the rag chain
+        rag_chain=create_retrieval_chain(history_aware_retriever,question_answer_chain)
+
+        return rag_chain
+    except Exception as e:
+        return str(e)
+
+
+def get_response(rag_chain,query):
+    """
+    This fun is responsible for continuing the chat
+    
+    Args:
+        rag_chain (object): The rag chain object
+        query (str): The user query
+    return:
+        response (str): The response from the chatbot
+    """
+    try:
+        st.session_state['chat_history'].append(HumanMessage(content=query))
+        response=rag_chain.invoke({
+            "input":query,'chat_history':st.session_state['chat_history']
+        })
+        st.session_state['chat_history'].append(SystemMessage(content=response['answer']))
+
+        return response['answer']
+            
+    except Exception as e:
+        print(str(e))
+
+
+def load_previous_chat():
+    for i in range(len(st.session_state['chat_history'])):
+        if i%2==0:
+            st.write(f'💬 You: {st.session_state['chat_history'][i].content}') 
+        else:
+            st.write(f'🤖 Bot: {st.session_state['chat_history'][i].content}')  
+
+def stream_data(response):
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+def save_notes(file,respone):
+    """
+    This fun is responsible for saving the notes
+    """
+    with open(file,"a+") as f:
+        f.write(f"{respone}")
